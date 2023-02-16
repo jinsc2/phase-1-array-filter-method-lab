c70db1cfc15b8138823b3b052f9a0b286be16334
Jinwoong <jinsc2@gmail.com>
2023-02-15 20:26:46 -0500

    commit tree 65030099889f739641376607d00ef7eb88a867eb

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// // Code your solution here
+// function findMatching (name, string){
+//     return name.filter(
+//         (someMatch) => someMatch.toLowerCase() === string.toLowerCase()
+//     );
+// }
+
+let findMatching = (name, string) => name.filter(
+    (someMatch) => someMatch.toLowerCase() === string.toLowerCase()
+);
+
+function fuzzyMatch (name, string){
+    return name.filter(
+        (someMatch) => someMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+} 
+
+function matchName (objects, string){
+    return objects.filter((record) => record.name === string);
+}
